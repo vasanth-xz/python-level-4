fc03bf19ce8a69c2d32f6e6b7b7982cfa5f833fa
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-10-06 11:02:18 +0530

    program 17b

---
+++ ex.17 b.py
@@ -0,0 +1,11 @@
+print("sum of N Number")
+print("---------------")
+a=int(input("Enter the Starting Number:"))
+n=int(input("Enter the Ending Number:"))
+print("result")
+Sum=0
+for i in range(a,n+1):
+     print("+",+i)
+     Sum=Sum+i;
+print("sum Value is",Sum)
+
