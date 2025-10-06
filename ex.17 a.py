a74967aa55e248e4823fd2bcb31d882780d31d70
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-10-06 11:01:40 +0530

    program 17 a

---
+++ ex.17 a.py
@@ -0,0 +1,10 @@
+print("sum of N Number")
+print("---------------")
+n=int(input("Enter the Number:"))
+print("result")
+Sum=0
+for i in range(1*n+1):
+     print("+",+i)
+     Sum=Sum+i;
+print("sum")
+print(Sum)
