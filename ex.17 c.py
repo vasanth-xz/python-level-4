9d72cd2e7be79a0b3aa30a52a411cb8c5d235db7
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-10-06 11:02:41 +0530

    Add files via upload

---
+++ ex.17 c.py
@@ -0,0 +1,17 @@
+print("sum of N Number")
+print("---------------")
+a=int(input("Enter the Starting Number:"))
+b=int(input("Enter the Ending Number:"))
+c=int(input("Enter the Difference:"))
+print("result")
+print("-------")
+print("Series")
+count=0
+sum=0
+for i in range(a,b+1,c):
+     print("+",+i)
+     Sum=Sum+i;
+     count++ ; 
+print("sum Value:",Sum)
+print("count value:",count)
+
