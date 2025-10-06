80e1f60abad8e619bc1aa6e7237a79f2d2b14964
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-10-06 11:00:58 +0530

    program 17

---
+++ ex17.py
@@ -0,0 +1,10 @@
+print("Sum of N Numbers")
+print("----------------")
+n=int(input("Enter The Number:"))
+print("Result")
+sum=0
+for i in range(1,n+1):
+    print("+",+i)
+    sum=sum+i;
+print("Sum")
+print(sum)
